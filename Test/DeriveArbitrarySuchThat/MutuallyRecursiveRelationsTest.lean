e7fd2c179d36bb287d485bb7b6fad98ad97ffee2
ngernest <eyn5@cornell.edu>
2025-08-13 16:25:52 -0400

    Update mutually recursive relations test

--- Test/DeriveArbitrarySuchThat/MutuallyRecursiveRelationsTest.lean
+++ Test/DeriveArbitrarySuchThat/MutuallyRecursiveRelationsTest.lean
@@ -14,26 +14,14 @@ mutual
     | succ_of_even_is_odd : ∀ n : Nat, Even n → Odd (.succ n)
 end
 
-/--
-info: Try this generator: instance : ArbitrarySizedSuchThat Nat (fun n_1 => Odd n_1) where
-  arbitrarySizedST :=
-    let rec aux_arb (initSize : Nat) (size : Nat) : OptionT Plausible.Gen Nat :=
-      match size with
-      | Nat.zero =>
-        OptionTGen.backtrack
-          [(1, do
-              let n ← ArbitrarySizedSuchThat.arbitrarySizedST (fun n => Even n) initSize;
-              return Nat.succ n)]
-      | Nat.succ size' =>
-        OptionTGen.backtrack
-          [(1, do
-              let n ← ArbitrarySizedSuchThat.arbitrarySizedST (fun n => Even n) initSize;
-              return Nat.succ n),
-            ]
-    fun size => aux_arb size size
--/
-#guard_msgs(info, drop warning) in
-#derive_generator (fun (n : Nat) => Odd n)
+/-- To make the derived generators below compile, we need to
+    manually add a dummy instance of `ArbitrarySizedSuchThat` for one of the relations, since Lean doesn't support
+    mutually recursively typeclass instances currently.
+
+    Note that the instance of `ArbitrarySizedSuchThat` for `Odd` below (produced by `#derive_generator`)
+    will shadow this one -- it takes precedence over this dummy instance. -/
+instance : ArbitrarySizedSuchThat Nat (fun n => Odd n) where
+  arbitrarySizedST (_ : Nat) := return 1
 
 /--
 info: Try this generator: instance : ArbitrarySizedSuchThat Nat (fun n_1 => Even n_1) where
@@ -57,3 +45,24 @@ info: Try this generator: instance : ArbitrarySizedSuchThat Nat (fun n_1 => Even
 -/
 #guard_msgs(info, drop warning) in
 #derive_generator (fun (n : Nat) => Even n)
+
+/--
+info: Try this generator: instance : ArbitrarySizedSuchThat Nat (fun n_1 => Odd n_1) where
+  arbitrarySizedST :=
+    let rec aux_arb (initSize : Nat) (size : Nat) : OptionT Plausible.Gen Nat :=
+      match size with
+      | Nat.zero =>
+        OptionTGen.backtrack
+          [(1, do
+              let n ← ArbitrarySizedSuchThat.arbitrarySizedST (fun n => Even n) initSize;
+              return Nat.succ n)]
+      | Nat.succ size' =>
+        OptionTGen.backtrack
+          [(1, do
+              let n ← ArbitrarySizedSuchThat.arbitrarySizedST (fun n => Even n) initSize;
+              return Nat.succ n),
+            ]
+    fun size => aux_arb size size
+-/
+#guard_msgs(info, drop warning) in
+#derive_generator (fun (n : Nat) => Odd n)
