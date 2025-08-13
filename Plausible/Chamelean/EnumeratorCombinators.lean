484e9997f99c4c9faf3e8891f37085c686720c77
ngernest <eyn5@cornell.edu>
2025-08-13 16:03:21 -0400

    Add bounds to EnumeratorCombinators.enumerate

--- Plausible/Chamelean/EnumeratorCombinators.lean
+++ Plausible/Chamelean/EnumeratorCombinators.lean
@@ -36,7 +36,7 @@ def enumerateFuel (fuel : Nat) (total : Nat) (es : List (OptionT Enumerator α))
 /-- Tries all enumerators from a list until one returns a `Some` value or all the enumerators have
     failed once with `None`. -/
 def enumerate (es : List (OptionT Enumerator α)) : OptionT Enumerator α :=
-  enumerateFuel es.length es.length es
+  enumerateFuel (fuel := min es.length 10) (total := es.length) es
 
 /-- Applies the checker `f` to a `LazyList l` of values, returning the resultant `Option Bool`
     (the parameter `anyNone` is used to indicate whether any of the elements examined previously have been `none`) -/
