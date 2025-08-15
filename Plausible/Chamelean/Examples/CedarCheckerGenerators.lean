4cdb14c55a83c34b9ab58dc84df5ffb71147b2b0
ngernest <eyn5@cornell.edu>
2025-08-15 13:31:42 -0400

    Checkers/generators for SetEntityValues / DefinedNames

--- Plausible/Chamelean/Examples/CedarCheckerGenerators.lean
+++ Plausible/Chamelean/Examples/CedarCheckerGenerators.lean
@@ -139,3 +139,155 @@ info: Try this generator: instance : ArbitrarySizedSuchThat CedarExpr (fun ce_1 
 -/
 #guard_msgs(info, drop warning) in
 #derive_generator (fun (ce : CedarExpr) => SetExpr ce)
+
+/--
+info: Try this checker: instance : DecOpt (SetEntityValues ce_1) where
+  decOpt :=
+    let rec aux_dec (initSize : Nat) (size : Nat) (ce_1 : CedarExpr) : Option Bool :=
+      match size with
+      | Nat.zero =>
+        DecOpt.checkerBacktrack
+          [fun _ =>
+            match ce_1 with
+            | CedarExpr.setExprNil => Option.some Bool.true
+            | _ => Option.some Bool.false]
+      | Nat.succ size' =>
+        DecOpt.checkerBacktrack
+          [fun _ =>
+            match ce_1 with
+            | CedarExpr.setExprNil => Option.some Bool.true
+            | _ => Option.some Bool.false,
+            fun _ =>
+            match ce_1 with
+            | CedarExpr.setExprCons (CedarExpr.lit (Prim.entityUID uid)) r => aux_dec initSize size' r
+            | _ => Option.some Bool.false]
+    fun size => aux_dec size size ce_1
+-/
+#guard_msgs(info, drop warning) in
+#derive_checker (SetEntityValues ce)
+
+/--
+info: Try this generator: instance : ArbitrarySizedSuchThat CedarExpr (fun ce_1 => SetEntityValues ce_1) where
+  arbitrarySizedST :=
+    let rec aux_arb (initSize : Nat) (size : Nat) : OptionT Plausible.Gen CedarExpr :=
+      match size with
+      | Nat.zero => OptionTGen.backtrack [(1, return CedarExpr.setExprNil)]
+      | Nat.succ size' =>
+        OptionTGen.backtrack
+          [(1, return CedarExpr.setExprNil),
+            (Nat.succ size', do
+              let r ← aux_arb initSize size';
+              do
+                let uid ← Plausible.Arbitrary.arbitrary;
+                return CedarExpr.setExprCons (CedarExpr.lit (Prim.entityUID uid)) r)]
+    fun size => aux_arb size size
+-/
+#guard_msgs(info, drop warning) in
+#derive_generator (fun (ce : CedarExpr) => SetEntityValues ce)
+
+/--
+info: Try this checker: instance : DecOpt (DefinedName ns_1 n_1) where
+  decOpt :=
+    let rec aux_dec (initSize : Nat) (size : Nat) (ns_1 : List EntityName) (n_1 : EntityName) : Option Bool :=
+      match size with
+      | Nat.zero =>
+        DecOpt.checkerBacktrack
+          [fun _ =>
+            match ns_1 with
+            | List.cons A L => DecOpt.decOpt (Eq A n_1) initSize
+            | _ => Option.some Bool.false]
+      | Nat.succ size' =>
+        DecOpt.checkerBacktrack
+          [fun _ =>
+            match ns_1 with
+            | List.cons A L => DecOpt.decOpt (Eq A n_1) initSize
+            | _ => Option.some Bool.false,
+            fun _ =>
+            match ns_1 with
+            | List.cons B L =>
+              DecOpt.andOptList [DecOpt.decOpt (Eq (bne n_1 B) (Bool.true)) initSize, aux_dec initSize size' L n_1]
+            | _ => Option.some Bool.false]
+    fun size => aux_dec size size ns_1 n_1
+-/
+#guard_msgs(info, drop warning) in
+#derive_checker (DefinedName ns n)
+
+/--
+info: Try this generator: instance : ArbitrarySizedSuchThat EntityName (fun n_1 => DefinedName ns_1 n_1) where
+  arbitrarySizedST :=
+    let rec aux_arb (initSize : Nat) (size : Nat) (ns_1 : List EntityName) : OptionT Plausible.Gen EntityName :=
+      match size with
+      | Nat.zero =>
+        OptionTGen.backtrack
+          [(1,
+              match ns_1 with
+              | List.cons A L => do
+                let n_1 ← ArbitrarySizedSuchThat.arbitrarySizedST (fun n_1 => Eq A n_1) initSize;
+                return n_1
+              | _ => OptionT.fail)]
+      | Nat.succ size' =>
+        OptionTGen.backtrack
+          [(1,
+              match ns_1 with
+              | List.cons A L => do
+                let n_1 ← ArbitrarySizedSuchThat.arbitrarySizedST (fun n_1 => Eq A n_1) initSize;
+                return n_1
+              | _ => OptionT.fail),
+            (Nat.succ size',
+              match ns_1 with
+              | List.cons B L => do
+                let n_1 ← aux_arb initSize size' L;
+                match DecOpt.decOpt (Eq (bne n_1 B) (Bool.true)) initSize with
+                  | Option.some Bool.true => return n_1
+                  | _ => OptionT.fail
+              | _ => OptionT.fail)]
+    fun size => aux_arb size size ns_1
+-/
+#guard_msgs(info, drop warning) in
+#derive_generator (fun (n : EntityName) => DefinedName ns n)
+
+/--
+info: Try this checker: instance : DecOpt (DefinedNames ns_1 ns0_1) where
+  decOpt :=
+    let rec aux_dec (initSize : Nat) (size : Nat) (ns_1 : List EntityName) (ns0_1 : List EntityName) : Option Bool :=
+      match size with
+      | Nat.zero =>
+        DecOpt.checkerBacktrack
+          [fun _ =>
+            match ns0_1 with
+            | List.nil => Option.some Bool.true
+            | _ => Option.some Bool.false]
+      | Nat.succ size' =>
+        DecOpt.checkerBacktrack
+          [fun _ =>
+            match ns0_1 with
+            | List.nil => Option.some Bool.true
+            | _ => Option.some Bool.false,
+            fun _ =>
+            match ns0_1 with
+            | List.cons n ns0 =>
+              DecOpt.andOptList [DecOpt.decOpt (DefinedName ns_1 n) initSize, aux_dec initSize size' ns_1 ns0]
+            | _ => Option.some Bool.false]
+    fun size => aux_dec size size ns_1 ns0_1
+-/
+#guard_msgs(info, drop warning) in
+#derive_checker (DefinedNames ns ns0)
+
+/--
+info: Try this generator: instance : ArbitrarySizedSuchThat (List EntityName) (fun ns0_1 => DefinedNames ns_1 ns0_1) where
+  arbitrarySizedST :=
+    let rec aux_arb (initSize : Nat) (size : Nat) (ns_1 : List EntityName) : OptionT Plausible.Gen (List EntityName) :=
+      match size with
+      | Nat.zero => OptionTGen.backtrack [(1, return List.nil)]
+      | Nat.succ size' =>
+        OptionTGen.backtrack
+          [(1, return List.nil),
+            (Nat.succ size', do
+              let n ← ArbitrarySizedSuchThat.arbitrarySizedST (fun n => DefinedName ns_1 n) initSize;
+              do
+                let ns0 ← aux_arb initSize size' ns_1;
+                return List.cons n ns0)]
+    fun size => aux_arb size size ns_1
+-/
+#guard_msgs(info, drop warning) in
+#derive_generator (fun (ns0 : List EntityName) => DefinedNames ns ns0)
