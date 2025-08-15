95c89629cceadedc4da27f321c2002037324355a
ngernest <eyn5@cornell.edu>
2025-08-15 11:27:30 -0400

    Add Cedar entities

--- Plausible/Chamelean/Examples/Cedar.lean
+++ Plausible/Chamelean/Examples/Cedar.lean
@@ -1,3 +1,6 @@
+-- Import linter from batteries to suppress "missing documentation" linter warnings
+import Batteries.Tactic.Lint
+
 ------------------------------------
 -- Part One: Cedar expression syntax
 -------------------------------------
@@ -5,12 +8,12 @@
 /-- The name of an entity -/
 inductive EntityName where
 | MkName : String → List String → EntityName
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
 /-- Entity UIDs -/
 inductive EntityUID where
 | MkEntityUID : EntityName → String → EntityUID
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
 /-- Primitive values -/
 inductive Prim where
@@ -18,7 +21,7 @@ inductive Prim where
 | int (i : Int)
 | stringLit (s : String)
 | entityUID (e : EntityUID)
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
 /-- Variables -/
 inductive Var where
@@ -26,13 +29,13 @@ inductive Var where
 | action
 | resource
 | context
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
 /-- Pattern elements -/
 inductive PatElem where
 | star
 | justLit (s : String)
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
 /-- Unary operations -/
 inductive UnaryOp where
@@ -40,7 +43,7 @@ inductive UnaryOp where
 | neg
 | like (p : List PatElem)
 | is (ety : EntityName)
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
 /-- Binary operations -/
 inductive BinaryOp where
@@ -54,7 +57,7 @@ inductive BinaryOp where
 | contains
 | containsAll
 | containsAny
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
 /-- Cedar expressions
     - Note: we "inlines" list constructors for sets and records to avoid issue with mutual recursion in a list/generic type -/
@@ -72,18 +75,18 @@ inductive Expr where
 | setExprCons (e : Expr) (ls : Expr)
 | recExprNil
 | recExprCons (s : String) (e : Expr) (attrs : Expr)
-deriving DecidableEq
+deriving BEq
 
 /-- Type of entity data.
     Precondition: the `Expr` argument should always be a record of values -/
 inductive EntityData where
 | MkEntityData : Expr → List EntityUID → EntityData
-deriving DecidableEq
+deriving BEq
 
 /-- given `MkReq P A R C`, assumes that `RecordExpr C` and `Value C` hold --/
 inductive Request where
 | MkReq : EntityUID → EntityUID → EntityUID → Expr → Request
-deriving DecidableEq
+deriving BEq
 
 -------------------------------------------------
 -- Part Two: Pretty Printing Cedar Expressions
@@ -252,12 +255,14 @@ inductive SetEntityValues : Expr → Prop where
 
 -- Types
 
+/-- Boolean types -/
 inductive BoolType where
 | anyBool
 | tt
 | ff
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
+/-- Types in Cedar -/
 inductive CedarType where
 | boolType (bty : BoolType)
 | intType
@@ -266,9 +271,157 @@ inductive CedarType where
 | setType (ty : CedarType)
 | recordTypeNil
 | recordTypeCons (s : String) (opt : Bool) (ty : CedarType) (rest : CedarType)
-deriving Repr, DecidableEq
+deriving Repr, BEq
 
 /-- Determines whether a `CedarType` is a `RecordType` -/
 inductive RecordType : CedarType → Prop where
 | RTNil : RecordType CedarType.recordTypeNil
 | RTCons : ∀ fn o T1 T2, RecordType (CedarType.recordTypeCons fn o T1 T2)
+
+@[nolint docBlame]
+inductive DefinedName : List EntityName → EntityName → Prop where
+| DNFound : ∀ L A B,
+    A = B →
+    DefinedName (A::L) B
+| DNRest : ∀ L A B,
+    ¬(A = B) →
+    DefinedName L A →
+    DefinedName (B::L) A
+
+@[nolint docBlame]
+inductive DefinedNames : List EntityName → List EntityName → Prop where
+| DNSNil : ∀ ns, DefinedNames ns []
+| DNSCons : ∀ n ns0 ns,
+    DefinedName ns n →
+    DefinedNames ns ns0 →
+    DefinedNames ns (n::ns0)
+
+/-- Inductive relation specifying well-formedness conditions for Cedar types -/
+inductive WfCedarType : List EntityName → CedarType → Prop where
+| WfBoolType : ∀ ns B, WfCedarType ns (CedarType.boolType B)
+| WfIntType : ∀ ns, WfCedarType ns CedarType.intType
+| WfStringType : ∀ ns, WfCedarType ns CedarType.stringType
+| WfEntityType : ∀ ns n,
+    DefinedName ns n →
+    WfCedarType ns (CedarType.entityType n)
+| WfSetType : ∀ T ns,
+    WfCedarType ns T →
+    WfCedarType ns (CedarType.setType T)
+| WfRecordTypeNil : ∀ ns, WfCedarType ns CedarType.recordTypeNil
+| WfRecordTypeConsNil : ∀ fn o T1 ns,
+    WfCedarType ns T1 →
+    WfCedarType ns (CedarType.recordTypeCons fn o T1 CedarType.recordTypeNil)
+| WfRecordTypeConsCons : ∀ fn o T1 ns fn1 o1 T2 r,
+    WfCedarType ns T1 →
+    WfCedarType ns (CedarType.recordTypeCons fn1 o1 T2 r) →
+    WfCedarType ns (CedarType.recordTypeCons fn o T1 (CedarType.recordTypeCons fn1 o1 T2 r))
+
+/-- Well-formed record types are types that are both well-formed and record types -/
+def WfRecordType (ns : List EntityName) (ct : CedarType) : Prop :=
+  WfCedarType ns ct ∧ RecordType ct
+
+
+-- Schemas
+
+@[nolint docBlame]
+inductive EntitySchemaEntry where
+| MkEntitySchemaEntry (ancestors : List EntityName) (attrs : List (String × Bool × CedarType))
+deriving Repr, BEq
+
+@[nolint docBlame]
+inductive WfAttrs : List EntityName → List (String × Bool × CedarType) → Prop where
+| WfAttrsNil : ∀ ns, WfAttrs ns []
+| WfAttrsCons : ∀ ns T s b attrs,
+    WfCedarType ns T →
+    WfAttrs ns attrs →
+    WfAttrs ns ((s, b, T)::attrs)
+
+@[nolint docBlame]
+inductive WfET : List EntityName → EntitySchemaEntry → Prop where
+| WfETSingle : ∀ ns ancs attrs,
+    DefinedNames ns ancs →
+    WfAttrs ns attrs →
+    WfET ns (EntitySchemaEntry.MkEntitySchemaEntry ancs attrs)
+
+@[nolint docBlame]
+inductive WfETS : List EntityName → List EntityName → List (EntityName × EntitySchemaEntry) → Prop where
+| WfETSSingle : ∀ ns n et,
+    DefinedName ns n →
+    WfET ns et →
+    WfETS ns [n] [(n, et)]
+| WfETSCons : ∀ n ns ns0 et ets,
+    DefinedName ns n →
+    WfET ns et →
+    WfETS ns ns0 ets →
+    WfETS ns (n::ns0) ((n, et)::ets)
+
+@[nolint docBlame]
+inductive ActionSchemaEntry where
+| MkActionSchemaEntry (prin : List EntityName) (res : List EntityName) (contextType : List (String × Bool × CedarType))
+deriving Repr, BEq
+
+/-- LATER: Allow more than one principal and resource -/
+inductive WfACT : List EntityName → (EntityUID × ActionSchemaEntry) → Prop where
+| WfACTSingle : ∀ n p r ns s attrs,
+    DefinedName ns n →
+    DefinedName ns p →
+    DefinedName ns r →
+    WfAttrs ns attrs →
+    WfACT ns ((EntityUID.MkEntityUID n s), (ActionSchemaEntry.MkActionSchemaEntry [p] [r] attrs))
+
+@[nolint docBlame]
+inductive WfACTS : List EntityName → List (EntityUID × ActionSchemaEntry) → Prop where
+| WfACTSSingle : ∀ ns act,
+    WfACT ns act →
+    WfACTS ns [act]
+| WfACTSCons : ∀ ns act acts,
+    WfACT ns act →
+    WfACTS ns acts →
+    WfACTS ns (act::acts)
+
+@[nolint docBlame]
+inductive Schema where
+| MkSchema (ets : List (EntityName × EntitySchemaEntry)) (acts : List (EntityUID × ActionSchemaEntry))
+deriving Repr, BEq
+
+@[nolint docBlame]
+inductive WfSchema : List EntityName → Schema → Prop where
+| WfS : ∀ ns ets acts,
+    WfETS ns ns ets →
+    WfACTS ns acts →
+    WfSchema ns (Schema.MkSchema ets acts)
+
+@[nolint docBlame]
+inductive DefinedEntity : List (EntityName × EntitySchemaEntry) → EntityName → Prop where
+| DENow : ∀ n E R, DefinedEntity ((n, E)::R) n
+| DELater : ∀ n n1 E R,
+     ¬(n = n1) →
+    DefinedEntity R n →
+    DefinedEntity ((n1, E)::R) n
+
+@[nolint docBlame]
+inductive DefinedEntities : List (EntityName × EntitySchemaEntry) → List EntityName → Prop where
+| DESNil : DefinedEntities [] []
+| DESCons : ∀ n ns et ets,
+    DefinedEntities ets ns →
+    DefinedEntities ((n, et)::ets) (n::ns)
+
+/-- NB: Ideally the string*bool parameter would be two distinct parameters.
+    It's written this way due to current QuickChick/Chamelean limitations on generation. -/
+inductive LookupEntityAttr : List (String × Bool × CedarType) → (String × Bool) → CedarType → Prop where
+| LUNow : ∀ F B FS TF,
+    LookupEntityAttr ((F, B, TF)::FS) (F, B) TF
+| LULater : ∀ F1 B1 F2 FS TF B,
+    ¬(F1 = F2) →
+    LookupEntityAttr FS (F1, B1) TF →
+    LookupEntityAttr ((F2, B, TF)::FS) (F1, B1) TF
+
+@[nolint docBlame]
+inductive GetEntityAttr : List (EntityName × EntitySchemaEntry) → (EntityName × String × Bool) → CedarType → Prop where
+| GENow : ∀ n fn b A E R T,
+    LookupEntityAttr E (fn, b) T →
+    GetEntityAttr ((n, (EntitySchemaEntry.MkEntitySchemaEntry A E))::R) (n, fn, b) T
+| GELater : ∀ n n1 fn b E R T,
+    ¬(n = n1) →
+    GetEntityAttr R (n, fn, b) T →
+    GetEntityAttr ((n1, E)::R) (n, fn, b) T
