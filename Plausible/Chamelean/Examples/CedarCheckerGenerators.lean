96c8f95884da451c0be332f10ff51470bbe7fde9
ngernest <eyn5@cornell.edu>
2025-08-15 11:49:04 -0400

    Derive Arbitrary instances for types modelling Cedar types/exprs

--- Plausible/Chamelean/Examples/CedarCheckerGenerators.lean
+++ Plausible/Chamelean/Examples/CedarCheckerGenerators.lean
@@ -1,6 +1,19 @@
 import Plausible.Chamelean.Examples.Cedar
 import Plausible.Arbitrary
 import Plausible.DeriveArbitrary
+import Plausible.Chamelean.GeneratorCombinators
 import Plausible.Chamelean.ArbitrarySizedSuchThat
 import Plausible.Chamelean.DeriveChecker
 import Plausible.Chamelean.DeriveConstrainedProducer
+
+open Plausible
+
+/-- We override the default `Arbitrary` instance for `String`s with our custom generator -/
+instance : Arbitrary String where
+  arbitrary := GeneratorCombinators.elementsWithDefault
+    "Aaron" ["Aaron", "John", "Mike", "Kesha", "Hicks", "A", "B", "C", "D"]
+
+-- Derive `Arbitrary` instances for datatypes modelling Cedar data/types/expressions
+deriving instance Arbitrary for
+  EntityName, EntityUID, Prim, Var, PatElem, UnaryOp, BinaryOp, Expr,
+  Request, BoolType, CedarType
